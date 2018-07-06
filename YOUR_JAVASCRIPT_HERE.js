1dbaad932d178cc4dc11545cc06930413dcc40db
Annemarie Beemsterboer <annemariebeemsterboer@live.nl>
2018-07-06 20:13:32 +0200

    first several steps

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,37 @@
 // Write your JS here
+
+
+
+let hero = {
+    name: 'Superman',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {type:'sword', damage: 2}
+}
+
+
+function rest(hero) {
+    while (hero.health < 10) {
+    hero.health = 10
+    // console.log('health restored')
+    }
+    return hero;
+}
+
+rest(hero)
+
+function pickUpItem (yourHero, yourWeapon) { // dont know the parameters??
+    hero.inventory.push(hero.weapon)
+    // console.log('item picked up')
+}
+
+pickUpItem (hero, hero.weapon)
+// console.log(hero.inventory)
+
+function equipWeapon (thePicker) {
+    hero.inventory.unshift(hero.weapon)
+    // console.log('weapon equiped')
+}
+
+equipWeapon(hero)
