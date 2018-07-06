ec64bdde77ced865d3d318382eb374afed00ca35
Annemarie Beemsterboer <annemariebeemsterboer@live.nl>
2018-07-06 23:06:20 +0200

    up until Picking up items

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,14 +1,18 @@
 // Write your JS here
 
-
-
 let hero = {
     name: 'Superman',
     heroic: true,
     inventory: [],
     health: 10,
-    weapon: {type:'sword', damage: 2}
+    weapon: [
+        {type: 'sword', damage: 2},
+    ]
 }
+
+let newWeapon = {type: 'dagger', damage: 2}
+hero.weapon.push(newWeapon)
+// console.log(hero.weapon)
 
 
 function rest(hero) {
@@ -21,17 +25,17 @@ function rest(hero) {
 
 rest(hero)
 
-function pickUpItem (yourHero, yourWeapon) { // dont know the parameters??
-    hero.inventory.push(hero.weapon)
-    // console.log('item picked up')
+function pickUpItem (yourHero, yourWeapon) {
+    hero.inventory.push(hero.weapon[0])
 }
 
-pickUpItem (hero, hero.weapon)
+pickUpItem();
 // console.log(hero.inventory)
 
-function equipWeapon (thePicker) {
-    hero.inventory.unshift(hero.weapon)
-    // console.log('weapon equiped')
-}
 
-equipWeapon(hero)
+// function equipWeapon(yourHero) {
+//     hero.inventory.unshift(hero.weapon)
+//     // console.log('weapon equiped')
+// }
+
+// equipWeapon(hero)
